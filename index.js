b9631e3a5a5333dfe442ec017c972b865d7cfdbb
Peter Žnidar <5er.znidar@gmail.com>
2022-07-26 18:10:45 +0200

    v1.0 of backend server

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
 'use strict';
 
-var os = require('os');
-var nodeStatic = require('node-static');
 var http = require('http');
+
+var os = require('os');
 var socketIO = require('socket.io');
+//var nodeStatic = require('node-static');
 
-rooms = {};
+var https = require('https');
+var path = require('path');
+var express = require('express');
+var fs = require('fs');
 
-var fileServer = new(nodeStatic.Server)({
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET'
-  }
+
+var app = express();
+app.use(express.static('js'));
+app.use(express.static('css'));
+
+app.get('/client', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+  console.log('p>> getting client');
+  //res.send('Open client');
 });
-var app = http.createServer(function(req, res) {
-  fileServer.serve(req, res);
-}).listen(8080);
+app.get('/host', function(req, res) {
+  res.sendFile(path.join(__dirname, '/host_index.html'));
+  console.log('p>> getting host');
+  //res.send('Open host');
+});
+app.get('/test', function(req, res) {
+  console.log('p>> getting test');
+  res.send('Test is succesful');
+});
 
-var io = socketIO.listen(app);
+var privateKey  = fs.readFileSync('key.pem', 'utf8');
+var certificate = fs.readFileSync('cert.pem', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(443);
+
+
+var rooms = {};
+var io = socketIO.listen(httpsServer);
 io.sockets.on('connection', function(socket) {
 
   // convenience function to log server messages on the client
@@ -27,18 +49,31 @@ io.sockets.on('connection', function(socket) {
     socket.emit('log', array, socket.id);
   }
 
-  socket.on('message', function(message, room, role) {
-    log('Client said: ', message);
-    /* for a real app, would be room-only (not broadcast)
-    if (socket.id != rooms[room]) {
-      io.to(rooms[room]).emit('message', message, room, role);
+  socket.on('message', function(message, room, id) {
+    // for a real app, would be room-only (not broadcast)
+    if (message == 'got user media') {
+      socket.emit('message', message, room, id);
+    } else if (message == 'bye') {
+      console.log(id + ' has disconnected from room ' + room)
+      if (id == rooms[room]) {
+        console.log(id + ' was host');
+        socket.to(room).emit('message', message, room, id);
+        console.log('deleting room ' + room);
+        delete rooms[room];
+      } else {
+        socket.to(rooms[room]).emit('message', message, room, id);
+      }
     } else {
-      socket.to(room).emit('message', message, room, role);
-    }*/
-    socket.broadcast.emit('message', message, room, role);
+      if (id != rooms[room]) {
+        io.to(rooms[room]).emit('message', message, room, id);
+      } else {
+        socket.to(room).emit('message', message, room, id);
+      }
+    }
+    //socket.broadcast.emit('message', message, room, role);
   });
 
-  socket.on('create or join', function(room) {
+  /*socket.on('create or join', function(room) {
     log('Received request to create or join room ' + room);
 
     var clientsInRoom = io.sockets.adapter.rooms[room];
@@ -48,18 +83,53 @@ io.sockets.on('connection', function(socket) {
     if (numClients === 0) {
       socket.join(room);
       rooms[room] = socket.id;
-      log('Client ID ' + socket.id + ' created room ' + room);
+      console.log('Client ID ' + socket.id + ' created room ' + room);
       socket.emit('created', room, socket.id);
 
-    } else if (numClients <= 2) {
-      log('Client ID ' + socket.id + ' joined room ' + room);
-      io.sockets.in(room).emit('join', room);
+    } else if (numClients <= 10) {
+      console.log('Client ID ' + socket.id + ' joined room ' + room);
+      io.sockets.in(room).emit('join', room, socket.id);
       socket.join(room);
-      socket.emit('joined', room, socket.id);
-      io.sockets.emit('ready');
+      socket.emit('joined', room);
+      //setTimeout(()=>{console.log('Waited');}, 10000);
+      socket.to(room).emit('ready', socket.id);
     } else { // max two clients
       socket.emit('full', room);
     }
+  });*/
+
+  socket.on('create', function(room) {
+    var clientsInRoom = io.sockets.adapter.rooms[room];
+    var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
+    log('Room ' + room + ' now has ' + numClients + ' client(s)');
+
+    if (!(room in rooms)) {
+      socket.join(room);
+      rooms[room] = socket.id;
+      console.log('Client ID ' + socket.id + ' created room ' + room);
+      socket.emit('created', room, socket.id);
+
+    }
+  });
+
+  socket.on('join', function(room, uName) {
+    var clientsInRoom = io.sockets.adapter.rooms[room];
+    var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
+    log('Room ' + room + ' now has ' + numClients + ' client(s)');
+    if (room in rooms) {
+      if (numClients <= 10) {
+        console.log('Client ID ' + socket.id + ' joined room ' + room);
+        io.sockets.in(room).emit('join', room, socket.id, uName);
+        socket.join(room);
+        socket.emit('joined', room);
+        socket.to(room).emit('ready', socket.id);
+      } else { // max two clients
+        socket.emit('full', room);
+      }
+    } else {
+      socket.to(socket.id).emit('message', 'Room '+room+' doesn\'t exist', room, -1);
+    }
+    
   });
 
   socket.on('ipaddr', function() {
@@ -75,6 +145,7 @@ io.sockets.on('connection', function(socket) {
 
   socket.on('bye', function(){
     console.log('received bye');
+    
   });
 
 });
