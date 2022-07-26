11f86f219855240e2bc3f0220fa2c54c07c85f9a
Peter Žnidar <5er.znidar@gmail.com>
2022-07-26 18:14:36 +0200

    Update host_main.js
    v1.0 of auxiliary JS functions for 'profesor'  UI

--- js/host_main.js
+++ js/host_main.js
@@ -3,11 +3,19 @@
 var isChannelReady = false;
 var isInitiator = false;
 var isStarted = false;
-var isfilled = false;
 var localStream;
-var pc;
-var remoteStream;
+//var pc;
+//var pc2;
+//var remoteStream;
+//var remoteStream2;
+//var isFree = true;
 var turnReady;
+var curActiveID;
+
+var pcs = {}; // socketID String -> peerconnection Object
+var remoteStreams = {}; // socketID String -> stream Object
+var remoteVideoDOMs = []; // i Integer (vezan na DOM objekt) -> socketID String
+var uNameID = {}; // socketID String -> username String
 
 var pcConfig = {
   'iceServers': [{
@@ -21,16 +29,21 @@ var sdpConstraints = {
   offerToReceiveVideo: true
 };
 
+addEventListener('beforeunload', event => {
+  hangup();
+})
+
 /////////////////////////////////////////////
 
 var room = 'foo';
 // Could prompt for room name:
-// room = prompt('Enter room name:');
+room = prompt('Enter room name:'); 
 
 var socket = io.connect();
 
 if (room !== '') {
-  socket.emit('create or join', room);
+  document.getElementById('headerText').innerText = 'Host of room '+room;
+  socket.emit('create', room);
   console.log('Attempted to create or  join room', room);
 }
 
@@ -43,9 +56,10 @@ socket.on('full', function(room) {
   console.log('Room ' + room + ' is full');
 });
 
-socket.on('join', function (room){
-  console.log('Another peer made a request to join room ' + room);
+socket.on('join', function (room, id, uName){
+  console.log('Another peer ' + id + ' made a request to join room ' + room);
   console.log('This peer is the initiator of room ' + room + '!');
+  uNameID[id] = uName;
   isChannelReady = true;
 });
 
@@ -54,52 +68,57 @@ socket.on('joined', function(room) {
   isChannelReady = true;
 });
 
-socket.on('log', function(array) {
+socket.on('log', function(array, id) {
   console.log.apply(console, array);
+  console.log('sent by ', id);
 });
 
-socket.on('ready', function () {
-  console.log('recived ready signal');
+socket.on('ready', function (id) {
+  console.log('recived ready signal triggered by ', id);
+  maybeStart(id);
 });
+
 
 ////////////////////////////////////////////////
 
 function sendMessage(message) {
   console.log('Client sending message: ', message, room);
-  socket.emit('message', message, room, 'client');
+  socket.emit('message', message, room, socket.id);
 }
 
 // This client receives a message
-socket.on('message', function(message, room, role) {
-  console.log('Client received message:', message, '\nin ', room, ' by ', role);
+socket.on('message', function(message, room, id) {
+  console.log('Client received message:', message, '\nin ', room, ' by ', id);
   if (message === 'got user media') {
-    maybeStart();
-  } else if (message.type === 'offer') {
-    if (!isInitiator && !isStarted) {
-      maybeStart();
-    }
-    pc.setRemoteDescription(new RTCSessionDescription(message));
-    doAnswer();
+    maybeStart(id);
+    console.log('step 2: ACK')
   } else if (message.type === 'answer' && isStarted) {
-    pc.setRemoteDescription(new RTCSessionDescription(message));
+    //maybeStart(id);
+    pcs[id].setRemoteDescription(new RTCSessionDescription(message));
   } else if (message.type === 'candidate' && isStarted) {
     var candidate = new RTCIceCandidate({
       sdpMLineIndex: message.label,
       candidate: message.candidate
     });
-    pc.addIceCandidate(candidate);
+    pcs[id].addIceCandidate(candidate);
+    /*if (message.candidate == '') {
+      console.log('isFree ', isFree, ' to ', false);
+      isFree = false;
+    }*/
+    
   } else if (message === 'bye' && isStarted) {
-    handleRemoteHangup();
+    handleRemoteHangup(id);
   }
 });
 
-////////////////////////////////////////////////////
+//////////////////////////////////////////////////// encircledtofight
 
 var localVideo = document.querySelector('#localVideo');
 var remoteVideo = document.querySelector('#remoteVideo');
+var remoteStream2 = document.querySelector('#remoteVideo2');
 
 navigator.mediaDevices.getUserMedia({
-  audio: false,
+  audio: true,
   video: true
 })
 .then(gotStream)
@@ -113,7 +132,7 @@ function gotStream(stream) {
   localVideo.srcObject = stream;
   sendMessage('got user media');
   if (isInitiator) {
-    maybeStart();
+    maybeStart(socket.id);
   }
 }
 
@@ -129,17 +148,15 @@ if (location.hostname !== 'localhost') {
   );
 }
 
-function maybeStart() {
-  console.log('>>>>>>> maybeStart() ', isStarted, localStream, isChannelReady);
+function maybeStart(id) {
+  console.log('>>>>>>> maybeStart() ', localStream, isChannelReady, id);
   if (typeof localStream !== 'undefined' && isChannelReady) {
-    console.log('>>>>>> creating peer connection');
-    createPeerConnection();
-    pc.addStream(localStream);
+    
+    createPeerConnection(id);
+    pcs[id].addStream(localStream);
     isStarted = true;
-    console.log('isInitiator', isInitiator);
-    if (isInitiator) {
-      doCall();
-    }
+    console.log('maybeStart does call');
+    doCall(id);
   }
 }
 
@@ -149,12 +166,13 @@ window.onbeforeunload = function() {
 
 /////////////////////////////////////////////////////////
 
-function createPeerConnection() {
+function createPeerConnection(id) {
   try {
-    pc = new RTCPeerConnection(null);
-    pc.onicecandidate = handleIceCandidate;
-    pc.onaddstream = handleRemoteStreamAdded;
-    pc.onremovestream = handleRemoteStreamRemoved;
+    pcs[id] = new RTCPeerConnection(null);
+    pcs[id].onicecandidate = handleIceCandidate;
+    pcs[id].onaddstream = handleRemoteStreamAdded;
+    pcs[id].onremovestream = handleRemoteStreamRemoved;
+    console.log('pcs: ', pcs);
     console.log('Created RTCPeerConnnection');
   } catch (e) {
     console.log('Failed to create PeerConnection, exception: ' + e.message);
@@ -174,7 +192,6 @@ function handleIceCandidate(event) {
     });
   } else {
     console.log('End of candidates.');
-    isfilled = true;
   }
 }
 
@@ -182,23 +199,27 @@ function handleCreateOfferError(event) {
   console.log('createOffer() error: ', event);
 }
 
-function doCall() {
+function doCall(id) {
   console.log('Sending offer to peer');
-  pc.createOffer(setLocalAndSendMessage, handleCreateOfferError);
+  curActiveID = id;
+  pcs[id].createOffer(setLocalAndSendMessage, handleCreateOfferError);
 }
 
-function doAnswer() {
+function doAnswer(id) {
   console.log('Sending answer to peer.');
-  pc.createAnswer().then(
+  curActiveID = id;
+  pcs[id].createAnswer().then(
     setLocalAndSendMessage,
     onCreateSessionDescriptionError
-  );
+  );  
 }
 
 function setLocalAndSendMessage(sessionDescription) {
-  pc.setLocalDescription(sessionDescription);
+  console.log('setLocal ', curActiveID);
+  pcs[curActiveID].setLocalDescription(sessionDescription);
   console.log('setLocalAndSendMessage sending message', sessionDescription);
   sendMessage(sessionDescription);
+  curActiveID = undefined;
 }
 
 function onCreateSessionDescriptionError(error) {
@@ -235,9 +256,22 @@ function requestTurn(turnURL) {
 }
 
 function handleRemoteStreamAdded(event) {
-  console.log('Remote stream added.');
-  remoteStream = event.stream;
-  remoteVideo.srcObject = remoteStream;
+  var id = Object.keys(pcs).find(key => pcs[key] === this);
+  remoteStreams[id] = event.stream;
+  var len = remoteVideoDOMs.length;
+  remoteVideoDOMs[len] = id;
+  remoteVideo = document.getElementById('remoteVideo'+len);
+  remoteVideo.srcObject = remoteStreams[id];
+  
+  var videoLabel = document.getElementById('videoLabel'+len);
+  videoLabel.classList.remove('invisible');
+  videoLabel.innerHTML = uNameID[id];
+
+  console.log('post add');
+  console.log(0, pcs);
+  console.log(1, remoteStreams);
+  console.log(2, remoteVideoDOMs);
+  console.log(3, uNameID);
 }
 
 function handleRemoteStreamRemoved(event) {
@@ -250,14 +284,100 @@ function hangup() {
   sendMessage('bye');
 }
 
-function handleRemoteHangup() {
-  console.log('Session terminated.');
-  stop();
+function handleRemoteHangup(id) {
+  console.log('Connection terminated by ', id);
+  stop(id);
   isInitiator = false;
 }
 
-function stop() {
-  isStarted = false;
-  pc.close();
-  pc = null;
+function stop(id) {
+  // remove stopped data
+  remoteStreams[id] = null;
+  pcs[id].close();
+  pcs[id] = null;
+  delete pcs[id];
+  delete remoteStreams[id];
+  var index = remoteVideoDOMs.indexOf(id);
+  if (index>-1) {
+    remoteVideoDOMs.splice(index, 1);
+  }
+  delete uNameID[id];
+
+  // empty all elements
+  for (let i = 0; i < 12; i++) {
+    var stoppingVideo = document.querySelector('#remoteVideo'+i);
+    stoppingVideo.srcObject = null;
+    var stoppingLabel = document.querySelector('#videoLabel'+i);
+    stoppingLabel.classList.add('invisible');
+    stoppingLabel.innerHTML = '';
+  }
+
+  // sort and reapply to DOMs
+  for (let i = 0; i < remoteVideoDOMs.length; i++) {
+    var sID = remoteVideoDOMs[i];
+    document.querySelector('#remoteVideo'+i).srcObject = remoteStreams[sID];
+    var addLabel = document.querySelector('#videoLabel'+i);
+    addLabel.classList.remove('invisible');
+    addLabel.innerHTML = uNameID[sID];
+  }
+
+  console.log('post remove')
+  console.log(0, pcs);
+  console.log(1, remoteStreams);
+  console.log(2, remoteVideoDOMs);
+  console.log(3, uNameID);
+}
+
+function nameToPosition(elementName) {
+  var idStr = elementName.substr(11);
+  return parseInt(idStr);
+}
+
+function focusStream(domid) {
+  console.log(0, remoteStreams[remoteVideoDOMs[nameToPosition(domid)]]);
+  localVideo.srcObject = remoteStreams[remoteVideoDOMs[nameToPosition(domid)]];
+  document.querySelector('#btnUnFocus').classList.remove('disabled');
+}
+
+function unFocusStream() {
+  localVideo.srcObject = localStream;
+  document.querySelector('#btnUnFocus').classList.add('disabled');
+}
+
+function muteAllToggle() {
+  var button = document.getElementById('muteAll');
+  var button2 = document.getElementById('muteFocused');
+  if (button.classList.contains('fa-microphone')) {
+    // is active
+      // mute self on all
+      localStream.getAudioTracks()[0].enabled = false;
+      // update button to unmute
+      button.classList.remove('fa-microphone');
+      button.classList.remove('btn-success');
+      button.classList.add('fa-microphone-slash');
+      button.classList.add('btn-outline-danger');
+      // activate muteFocused button
+      button2.classList.remove('fa-microphone');
+      button2.classList.remove('btn-success');
+      button2.classList.add('fa-microphone-slash');
+      button2.classList.add('btn-outline-danger');
+      button2.classList.remove('disabled');
+  } else if (button.classList.contains('fa-microphone-slash')) {
+    // is deactivated
+      // unmute on all
+      localStream.getAudioTracks()[0].enabled = true;
+      // update button to unmute
+      button.classList.remove('fa-microphone-slash');
+      button.classList.remove('btn-outline-danger');
+      button.classList.add('fa-microphone');
+      button.classList.add('btn-success');
+      // deactivate muteFocused button
+      button2.classList.remove('fa-microphone-slash');
+      button2.classList.remove('btn-outline-danger');
+      button2.classList.add('fa-microphone');
+      button2.classList.add('btn-success');
+      button2.classList.add('disabled');
+  } else {
+    // error
+  }
 }
