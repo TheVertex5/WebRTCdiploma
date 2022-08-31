eae3067406c927d632c5e66d65359a10335cae30
Peter Žnidar <5er.znidar@gmail.com>
2022-08-31 21:08:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ httpsServer.listen(443);
 
 
 var rooms = {};
+var roomsToFile = {};
 var io = socketIO.listen(httpsServer);
 io.sockets.on('connection', function(socket) {
 
@@ -49,28 +50,87 @@ io.sockets.on('connection', function(socket) {
     socket.emit('log', array, socket.id);
   }
 
-  socket.on('message', function(message, room, id) {
+  socket.on('message', function(message, room, id, uName) {
     // for a real app, would be room-only (not broadcast)
     if (message == 'got user media') {
       socket.emit('message', message, room, id);
     } else if (message == 'bye') {
       console.log(id + ' has disconnected from room ' + room)
       if (id == rooms[room]) {
+        fs.appendFile(roomsToFile[room], 
+          'Host disconnected from room at '+(new Date())+'\n',
+          function() {
+            console.log('logged host disconnect')
+          });
         console.log(id + ' was host');
-        socket.to(room).emit('message', message, room, id);
+        socket.to(room).emit('message', message, room, id, uName);
         console.log('deleting room ' + room);
         delete rooms[room];
+        delete roomsToFile[room];
       } else {
-        socket.to(rooms[room]).emit('message', message, room, id);
+        socket.to(rooms[room]).emit('message', message, room, id, uName);
+        var uName = ''; //save Unames?
+        if (roomsToFile[room] != undefined) {
+          fs.appendFile(roomsToFile[room], 
+            'Client '+uName+' disconnected from room at '+(new Date())+'\n',
+            function() {
+              console.log('logged client disconnect')
+            });
+        }
       }
     } else {
       if (id != rooms[room]) {
-        io.to(rooms[room]).emit('message', message, room, id);
+        io.to(rooms[room]).emit('message', message, room, id, uName);
       } else {
-        socket.to(room).emit('message', message, room, id);
+        socket.to(room).emit('message', message, room, id, uName);
       }
     }
     //socket.broadcast.emit('message', message, room, role);
+  });
+
+  socket.on('chat', function (message, room, id, uName) {
+    fs.appendFile(roomsToFile[room], 
+      uName+' sent message '+message+' at '+(new Date())+'\n',
+      function() {
+        console.log('logged chat message')
+      });
+    socket.to(room).emit('chat', message, room, id, uName);
+  });
+
+  socket.on('upload', function(video, room, id, uName) {
+    if (id == rooms[room]) {
+      // do download
+      fs.writeFile('./video/'+room+'_'+dateString()+'/'+uName+'_'+room+'.mp4', 
+          video,
+          function() {
+            console.log('logged upload');
+          });
+      //console.log('video buffered ', video);
+    }
+  });
+
+  socket.on('status', function (message, room, id, uName) {
+    if (message == 'hand') {
+      
+      io.to(rooms[room]).emit('status', message, id, uName);
+
+      fs.appendFile(roomsToFile[room], 
+        uName+' raised hand at '+(new Date())+'\n',
+        function() {
+          console.log('logged raised hand')});
+    } else if (message == 'muteAll') {
+      console.log('send mute all');
+      socket.to(room).emit('status', 'mute', room, id, uName);
+    } else if (message == 'mute') {
+      console.log('send mute focused');
+      io.to(id).emit('status', message, room, rooms[room], uName)
+    } else if (message == 'unMuteAll') {
+      console.log('send unmute all');
+      socket.to(room).emit('status', 'unMute', room, id, uName);
+    } else if (message == 'unMute') {
+      console.log('send unmute focused');
+      io.to(id).emit('status', message, room, rooms[room], uName)
+    }
   });
 
   /*socket.on('create or join', function(room) {
@@ -108,7 +168,13 @@ io.sockets.on('connection', function(socket) {
       rooms[room] = socket.id;
       console.log('Client ID ' + socket.id + ' created room ' + room);
       socket.emit('created', room, socket.id);
-
+      roomsToFile[room] = './event_logs/'+room+'_'+dateString();
+      console.log(roomsToFile[room]);
+      fs.appendFile(roomsToFile[room], 
+          'Room '+room+' created on '+(new Date())+'\n',
+          function() {
+            console.log('logged create');
+          });
     }
   });
 
@@ -117,8 +183,13 @@ io.sockets.on('connection', function(socket) {
     var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
     log('Room ' + room + ' now has ' + numClients + ' client(s)');
     if (room in rooms) {
-      if (numClients <= 10) {
+      if (numClients <= 12) {
         console.log('Client ID ' + socket.id + ' joined room ' + room);
+        fs.appendFile(roomsToFile[room], 
+          'Client '+uName+' joined room at '+(new Date())+'\n',
+          function() {
+            console.log('logged client connect');
+          });
         io.sockets.in(room).emit('join', room, socket.id, uName);
         socket.join(room);
         socket.emit('joined', room);
@@ -149,3 +220,12 @@ io.sockets.on('connection', function(socket) {
   });
 
 });
+
+function dateString() {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+
+  return (mm + '-' + dd + '-' + yyyy);
+}
